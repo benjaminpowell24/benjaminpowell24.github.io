93f709bdff419e024e95680d313d478afa57ec04
benjaminpowell24 <benjaminpowell24@gmail.com>
2023-10-20 07:01:06 +0000

    Update portfolio

--- assets/js/platform.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    if (/iPad|iPhone|Mac OS X|iPod/.test(navigator.userAgent)) {
-        // $("<span>🧑🏾‍💻</span>").appendTo("#profile-icon");
-        $("<span>🚀</span>").appendTo("#project-icon");
-    } else {
-        console.log("This is not an iOS device!");
-    }
-});
