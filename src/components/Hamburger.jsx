36a2877c8d692c61850707cc161d6b61903df7ee
Benjamin Powell <benjaminpowell24@gmail.com>
2025-01-19 09:30:27 +0000

    feat: style hamburger menu items

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -20,20 +20,23 @@ const Hamburger = () => {
           className='w-1/2 md:w-1/3 lg:w-1/4 top-0 left-0 fixed h-screen py-2'
           style={{ backgroundColor: '#0d1118' }}
         >
-          <NavList.Item href=''>
-            <NavList.LeadingVisual>
+          <NavList.Item>
+            <NavList.LeadingVisual style={{ color: 'white' }}>
               <PersonFillIcon size={24} />
             </NavList.LeadingVisual>
             <NavList.TrailingAction
               label='close'
               icon={<XIcon />}
               onClick={() => setIsOpen(false)}
+              style={{ color: 'white' }}
             ></NavList.TrailingAction>
           </NavList.Item>
           <div className='my-5'>
             {menuItems.map((item) => (
-              <NavList.Item href={item.link}>
-                <NavList.LeadingVisual>{item.icon}</NavList.LeadingVisual>
+              <NavList.Item href={item.link} style={{ color: 'white' }}>
+                <NavList.LeadingVisual style={{ color: 'white' }}>
+                  {item.icon}
+                </NavList.LeadingVisual>
                 {item.name}
               </NavList.Item>
             ))}
